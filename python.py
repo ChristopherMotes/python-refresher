6173a351fcfd41cb871399e0fbb73e811702e3b2
Christopher Motes <motescj@aim.com>
2019-09-09 15:45:13 +0000

    okay, true false refreshed

--- python.py
+++ python.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 def funtionOne():
-    return True
+    return False
     
-
 if __name__ == "__main__":
     if funtionOne():
         print("test valid")
+    else:
+        print("test invalid")
     
