6d55027807868d0c854d30da6370713c0dce9122
Christopher Motes <motescj@aim.com>
2019-09-09 16:27:01 +0000

    pretty print new dictionary

--- python.py
+++ python.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 import json
 
-def funtionOne(dictionary):
-    print(dictionary)
+def functionOne(dictionary):
     for keys,values in dictionary.items():
         print("The key {} has the value {}".format(keys, values) )
         
-    dictionary.update({"number2": 2, "string2": "two"})
+    newDict = {"number2": 2, "string2": "two"}
+    print("adding new values to dictionary: {}".format(newDict))
+    try:
+        dictionary.update(newDict)
+    except:
+        raise Exception("Updating the dictionary failed")
         
-    return True
+    return dictionary
     
 if __name__ == "__main__":
     with open('/home/ec2-user/environment/python-refresher/file.json') as json_file:
         myDict = json.load(json_file)
-    if funtionOne(myDict):
-        print("test valid")
-    else:
-        print("test invalid")
+    try:
+        myDict = functionOne(myDict)
+        print(json.dumps(myDict, indent=4))
+    except:
+        raise 
     
